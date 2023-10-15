6efb7aba51a7a2198e7696072681d9f6587515b7
mrkumarrahul <Kumarrahulmahto1211@gmail.com>
2023-10-15 21:38:41 +0530

    Data type

---
+++ CodeWithHarry/L4DataType/index.js
@@ -0,0 +1,16 @@
+// NN SS BB U :- Shortcut to learn datatypes
+let a=null;               //null
+let b=34;               //Number
+let c="Rahul";           //String
+let d=Symbol("I am a nice symbol");         //symbol
+let e=true;                            //Boolean
+let f=BigInt('567')+BigInt('456');     //BigInt
+let g
+// Objects in js- Non-Primitives Data types
+const item={
+    "Harry":true,
+     Shubh:false,
+     Lovish:67,
+     Rohan:undefined
+}
+console.log(item["Harry"]);
