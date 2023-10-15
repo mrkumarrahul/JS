d869599ddc690edf09975c608093b1fdb4fc5b68
mrkumarrahul <Kumarrahulmahto1211@gmail.com>
2023-10-15 23:15:32 +0530

    Data Types

--- ChaiAurJavaScript/T9DataTypes/index.js
+++ ChaiAurJavaScript/T9DataTypes/index.js
@@ -1,5 +1,18 @@
+// Primitive datatypes
 const score=100;
 const scoreValue=100.3;
 const isLoggedIn=false;
 const outsideTemp=null
 let userEmail
+// Non Primitive Datatypes
+// Arrays,Objects,Functions
+const heros =["shaktiman","nagraj","doga"];
+let myObj={
+    name:"Rahul",
+    age:22
+}
+const myFunction=function() {
+    console.log("Hello World");
+}
+console.log(typeof userEmail)
+console.log(typeof myFunction);
