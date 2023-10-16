6f1158ce3296cef7480b6cab51c844747d558961
mrkumarrahul <Kumarrahulmahto1211@gmail.com>
2023-10-16 10:09:37 +0530

    Memory

---
+++ ChaiAurJavaScript/T10Memory/index.js
@@ -0,0 +1,14 @@
+let channel1="RahulVlog";
+let channel2=channel1;
+ channel2="RahulTutorial";
+ console.log(channel1);
+ console.log(channel2);
+ let user1={
+    name:"Rahul",
+    email:"Kumarrahulmahto1211@gmail.com"
+ }
+ let user2=user1;
+ user2.email="mahtorahulkumar1999@gmail.com"
+ console.log(user1);
+ console.log(user2);
+//  reference video: https://www.youtube.com/watch?v=7gwc-1czolw&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=10
