88ca97fe060a035fd9c744aa7dceb52e780a2479
mrkumarrahul <Kumarrahulmahto1211@gmail.com>
2023-10-20 17:40:41 +0530

    Try and Catch

---
+++ ApnaCollege/Day-1/V2/style.js
@@ -0,0 +1,13 @@
+// no error occured because 
+console.log("Hello");
+console.log("Hi");
+// let a=5;
+try{
+  console.log(a);
+} catch(err){
+    console.log(err);
+    console.log("a variable is not defined");
+}
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
