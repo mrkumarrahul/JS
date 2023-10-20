1c596baa3d72fcbc45d3c9e322134b786ddd070f
mrkumarrahul <Kumarrahulmahto1211@gmail.com>
2023-10-20 23:17:59 +0530

    Arrow function

---
+++ ApnaCollege/Day-1/V3/app.js
@@ -0,0 +1,12 @@
+const sum= (a,b) => {
+    console.log(a+b);
+}
+const multiply=(a,b) => {
+    console.log(a*b);
+}
+const cube=(n)=>{
+    return n*n*n;
+}
+const cubee=(c,d) => {
+    return c**d;
+}
