161707589a11763de0af005e66905c2d5e6cdc46
mrkumarrahul <Kumarrahulmahto1211@gmail.com>
2023-10-21 00:26:02 +0530

    SetTimeout

---
+++ ApnaCollege/Day-1/V4/app.js
@@ -0,0 +1,13 @@
+const name=()=>console.log("Rahul kumar Mahto");
+const multiply=(a,b) => 
+    a*b;
+
+const cube=(n) => 
+      n*n*n
+
+const square=(z) => 
+    z*z;
+
+const add=(c,d) => 
+    c+d;
+
---
+++ ApnaCollege/Day-1/V5/app.js
@@ -0,0 +1,5 @@
+console.log("Hello world");
+setTimeout( () => {
+  console.log("Lakshmi narayan college of Technology");
+},4000)
+console.log("Hello Duniya");
