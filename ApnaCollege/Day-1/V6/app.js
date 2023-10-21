d6e1450d9731bb446c677ddbdca9bdad4af618c2
mrkumarrahul <Kumarrahulmahto1211@gmail.com>
2023-10-21 11:12:18 +0530

    setInterval

---
+++ ApnaCollege/Day-1/V6/app.js
@@ -0,0 +1,11 @@
+console.log("Hello world");
+const id=setInterval(() => {
+   console.log("apna college");
+},2000)
+console.log("bye world");
+console.log(id);
+const id2=setInterval(() => {
+    console.log("apni kaksha");
+},2000)
+console.log(id2);
+clearInterval(id)
