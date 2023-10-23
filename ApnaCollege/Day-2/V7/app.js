7abfe3e6a2d5a75b4dd4c2e55c9f4810c401ad2e
mrkumarrahul <Kumarrahulmahto1211@gmail.com>
2023-10-24 01:08:16 +0530

    spread

---
+++ ApnaCollege/Day-2/V7/app.js
@@ -0,0 +1,5 @@
+function add(a,b=10)
+{
+    return a+b;
+}
+console.log(add(3));
---
+++ ApnaCollege/Day-2/V8/app.js
@@ -0,0 +1,6 @@
+let nums=[1,2,3,4,5,6,7,8,9,10]
+console.log(...nums)
+let min=Math.min(...nums)
+console.log(min);
+let max=Math.max(...nums)
+console.log(max);
