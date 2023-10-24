3e22f2d62cb28eef500e0677d79565f5013a965a
mrkumarrahul <Kumarrahulmahto1211@gmail.com>
2023-10-24 11:47:30 +0530

    spread

---
+++ ApnaCollege/Day-2/V9/app.js
@@ -0,0 +1,9 @@
+let nums=[1,2,3,4,5,6,7,8,9,10]
+let newNums=[...nums]
+console.log(newNums);
+let chars=[..."Rahul"]
+console.log(chars);
+let odd=[1,3,5,7,9]
+let even=[2,4,6,8,10]
+let num=[...odd,...even];
+// console.log(num);
