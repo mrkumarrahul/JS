61236280429136bc5f9dee2d08bdc9751aa8e68b
mrkumarrahul <Kumarrahulmahto1211@gmail.com>
2023-10-24 15:18:50 +0530

    spread with objects literal

---
+++ ApnaCollege/Day-2/V10/app.js
@@ -0,0 +1,9 @@
+let data={
+    email:"Kumarrahulmahto1211@gmail.com",
+    password:"abcd"
+};
+let dataCopy={...data,id:123}; 
+console.log(dataCopy);
+let nums=[1,2,3,4,5,6,7,8,9,10]
+let numCopy={...nums}
+console.log(numCopy);
