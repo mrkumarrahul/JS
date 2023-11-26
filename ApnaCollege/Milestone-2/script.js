43ee010ff950b6327c816918651fc311dcdfaa1a
mrkumarrahul <Kumarrahulmahto1211@gmail.com>
2023-11-26 23:07:13 +0530

    operators

--- ApnaCollege/Milestone-2/script.js
+++ ApnaCollege/Milestone-2/script.js
@@ -19,3 +19,54 @@ x +=5;
 console.log(x);
 y -=5;
 console.log(y);
+x%=2
+console.log(x);
+// comparison operator
+let c="3";
+let d="3";
+let e=5;
+let f="101";
+console.log("Is c is equal to d",c==d);
+console.log("Is c is not equal to d",c!=d);
+console.log("c==d",c==d);
+console.log("c===d",c===d);
+console.log("e==f",e==f);
+// logical operator
+let g=11;
+let h=10;
+console.log("g&&h",g>h&&g!=h);
+//ternary opertor
+let age=18;
+let result=age>=10?"adult":"not adult";
+console.log(result);
+// let name=prompt("Enter your name");
+// console.log(name);
+// let num=prompt("enter a number");
+// if(num%5===0)
+// {
+//   console.log("yes the number is multiple of 5");
+// }
+// else{
+//   console.log("No , the number is not multiple of 5");
+// }
+let marks=prompt("enter your marks");
+if(marks>=80&&marks<=100)
+{
+  console.log("Marks obtained Grade:A");
+}
+if(marks>=70&&marks<=79)
+{
+  console.log("Marks obtained Grade:B");
+}
+if(marks>=60&&marks<=69)
+{
+  console.log("Marks obtained Grade:C");
+}
+if(marks>=50&&marks<=59)
+{
+  console.log("Marks obtained Grade:E");
+}
+if(marks>=0&&marks<=49)
+{
+  console.log("Marks obtained Grade:F");
+}
---
+++ ApnaCollege/Milestone-2/script1.js
@@ -0,0 +1,33 @@
+//conditional statements
+let voter=28;
+if (voter>=18)
+{
+  console.log("you can vote");
+}
+else
+{
+  console.log("you can not vote");
+}
+let number=10;
+if(number%2==0)
+{
+  console.log("number is even",);
+}
+else{
+  console.log("number is odd");
+}
+//elseifelse
+let color="black";
+if(color=="white")
+{
+  console.log("not matched");
+}
+else if(color=="black")
+{
+  console.log("not matched");
+}
+else
+{
+  console.log("matched");
+}
+
