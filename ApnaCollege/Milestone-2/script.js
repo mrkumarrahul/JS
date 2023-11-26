e00e7c636bbbf32689130d3ce923b1197677b01e
mrkumarrahul <Kumarrahulmahto1211@gmail.com>
2023-11-26 16:23:11 +0530

    data-types and operators

---
+++ ApnaCollege/Milestone-2/script.js
@@ -0,0 +1,21 @@
+//arithmetic operators
+let a=15;
+let b=10;
+console.log(`${a}+${b}=`, a+b);
+console.log(`${a}-${b}=`, a-b);
+console.log(`${a}/${b}=`, a/b);
+console.log(`${a}%${b}=`, a%b);
+console.log(`${a}to the power${b}=`, a**b);
+console.log(`${a}%${b}=`, a%b);
+console.log("a++=",a++);
+console.log("++a=",++a);
+console.log("--a=",--a);
+console.log("a--=",a--);
+console.log(a);
+// assignment operator
+let x=10;
+let y=5;
+x +=5;
+console.log(x);
+y -=5;
+console.log(y);
